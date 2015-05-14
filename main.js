a127b3d85f99fae7b0f163f852b588a5a94bf353
eeshanl <eeshan@uw.edu>
2015-05-13 17:51:09 -0700

    edits

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function logIn() {
+
     var pass = document.getElementById("input").value;
     if (pass == "abcd") {
         window.location.replace('main.html');
     } else {
     	alert("Log in failed.");
+    	pass = "";
     }
 }
